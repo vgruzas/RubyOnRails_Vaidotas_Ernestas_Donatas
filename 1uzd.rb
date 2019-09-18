48acd8c9b440e4f82c5e2d085ae3eea98e1afb99
Vaidotas <vgruzas@gmail.com>
2019-09-18 21:25:59 +0300

    Update 1uzd.rb

--- 1uzd.rb
+++ 1uzd.rb
@@ -1,24 +1,24 @@
-puts Iveskite dvimacio masyvo eiluciu skaiciu 
+puts "Iveskite dvimacio masyvo eiluciu skaiciu:"
 size = gets.chomp.to_i
 array = Array.new(size) { Array.new(size) }
 arrayMin = Array.new(size)
 
-puts Is eiles, nuo virsutinio desiniojo kampo ivardinkite dvimacio masyvo elementus
+puts "Is eiles, nuo virsutinio desiniojo kampo ivardinkite dvimacio masyvo elementus."
 sum = 0.0
-size.times do i
-  size.times do j
+size.times do |i|
+  size.times do |j|
     array[i][j] = gets.chomp.to_i
     arrayMin[i] = array[i][i]
   end
 end
 
-puts Konvertuotas masyvas.
+puts "Konvertuotas masyvas:"
 
-size.times do i
-  size.times do j
-    if array[i][j]  arrayMin.min
+size.times do |i|
+  size.times do |j|
+    if array[i][j] > arrayMin.min
       array[i][j] = 'D'
-    elsif array[i][j]  arrayMin.min
+    elsif array[i][j] < arrayMin.min
       array[i][j] = 'M'
     else
       array[i][j] = 'L'
@@ -26,4 +26,4 @@ size.times do i
   end
 end
 
-puts array.map { x x.join( ) }
+puts array.map { |x| x.join( ) }
