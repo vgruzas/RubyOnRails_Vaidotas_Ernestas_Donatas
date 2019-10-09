f0421ecf217c1e3d28dfa9f63350cbe4dcaed669
Zajecc <zajcik.ez@gmail.com>
2019-10-09 12:41:34 +0300

    Fixed RuboCop functions

--- 3uzd/2uzd.rb
+++ 3uzd/2uzd.rb
@@ -1,3 +1,4 @@
+require 'cmath'
 def triangle(one, two, three)
   valid_triangle?(one, two, three)
 end
@@ -11,29 +12,32 @@ def valid_triangle?(one, two, three)
   end
 end
 
+# rubocop:disable Metrics/AbcSize
+
 def which_triangle?(one, two, three)
-  triangles = { TrikampisLygiakrastis: [60, 60, 60], TrikampisLygiasonis: [0],
-                TrikampisIvairiakrastis: [0] }
   angle_a = (Math.acos((two**2 + three**2 - one**2) / (2 * two * three).to_f) *
       180 / Math::PI).round(2)
   angle_b = (Math.acos((three**2 + one**2 - two**2) / (2 * three * one).to_f) *
-      180 / Math::PI)
-            .round(2)
+      180 / Math::PI).round(2)
   angle_c = (Math.acos((one**2 + two**2 - three**2) / (2 * one * two).to_f) *
-      180 / Math::PI)
-            .round(2)
-  if angle_a && angle_b == 60
-    return :TrikampisLygiakrastis, triangles[:TrikampisLygiakrastis]
+      180 / Math::PI).round(2)
+  which_triangle2(angle_a, angle_b, angle_c)
+end
 
+def which_triangle2(angle_a, angle_b, angle_c)
+  triangles = { Lygiakrast: [60, 60, 60], Lygiason: [0], Ivairiakrast: [0] }
+  if angle_a && angle_b == 60
+    [:TrikampisLygiakrastis, triangles[:TrikampisLygiakrastis]]
   elsif angle_a != angle_b && angle_b != angle_c
     triangles[:TrikampisIvairiakrastis] = [angle_a, angle_b, angle_c]
-    return :TrikampisIvairiakrastis, triangles[:TrikampisIvairiakrastis]
-
+    [:TrikampisIvairiakrastis, triangles[:TrikampisIvairiakrastis]]
   else
     triangles[:lygiasoniTrikampisLygiasoniss] = [angle_a, angle_b, angle_c]
-    return :TrikampisLygiasonis, triangles[:TrikampisLygiasonis]
+    [:TrikampisLygiasonis, triangles[:TrikampisLygiasonis]]
   end
 end
+# rubocop:enable Metrics/AbcSize
+
 puts 'Iveskite trys kraštinės:'
 krastine1 = gets.chomp.to_i
 krastine2 = gets.chomp.to_i
