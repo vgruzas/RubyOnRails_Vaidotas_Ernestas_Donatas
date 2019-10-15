7eeca083cee66ef36e58bba9e73c35e384719a95
DonSaik <ds32204@gmail.com>
2019-10-15 19:49:38 +0300

    Used RuboCop

--- 2uzd/2uzd.rb
+++ 2uzd/2uzd.rb
@@ -1,22 +1,23 @@
-puts "Iveskite masyvo elementu kieki:"
+# frozen_string_literal: true
+
+puts 'Iveskite masyvo elementu kieki:'
 n = gets.chomp.to_i
 array = Array.new(n)
-puts "Iveskite masyvo elementus: "
+puts 'Iveskite masyvo elementus: '
 n.times do |i|
   array[i] = gets.chomp.to_i
 end
 
-
 min = array.min
 max = array.max
 
-puts  " A) Min:" + min.to_s
-puts "Max: " + max.to_s
+puts  ' A) Min: ' + min.to_s
+puts 'Max: ' + max.to_s
 
-puts " B) Kiekis lygus min: " + array.count(min).to_s
-puts "Kiekis lygus max: " + array.count(max).to_s
+puts ' B) Kiekis lygus min: ' + array.count(min).to_s
+puts 'Kiekis lygus max: ' + array.count(max).to_s
 
-puts " C) Visi like elementai be max ir min: "
+puts ' C) Visi like elementai be max ir min: '
 
 array.delete(min)
 array.delete(max)
