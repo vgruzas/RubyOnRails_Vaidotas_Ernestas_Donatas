1930c15ea37b2ad507ab24e2020b25970b78b6b6
DonSaik <ds32204@gmail.com>
2019-10-23 09:55:32 +0300

    Changed tests

--- 2uzd/2uzd_test.rb
+++ 2uzd/2uzd_test.rb
@@ -8,6 +8,7 @@ class UzdTest < Test::Unit::TestCase
   def setup
     array = [-1, -1, 3, 5, 5, 5]
     @uzd = Uzd.new(array)
+    @empty = Uzd.new([])
   end
 
   def teardown
@@ -16,23 +17,30 @@ class UzdTest < Test::Unit::TestCase
 
   def test_min_value
     assert_equal(-1, @uzd.min_value)
+    assert_equal(nil, @empty.min_value)
   end
 
   def test_max_value
     assert_equal(5, @uzd.max_value)
+    assert_equal(nil, @empty.max_value)
   end
 
   def test_min_value_count
     assert_equal(2, @uzd.min_value_count)
+    assert_equal(0, @empty.min_value_count)
   end
 
   def test_max_value_count
     assert_equal(3, @uzd.max_value_count)
+    assert_equal(0, @empty.min_value_count)
   end
 
   def test_delete_min_max_value
     expected_array = [3]
     @uzd.delete_all_min_max
     assert_equal(expected_array, @uzd.array)
+    @empty.delete_all_min_max
+    expected_array = []
+    assert_equal(expected_array, @empty.array)
   end
 end
